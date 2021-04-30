83b2704e5bb67f2accf2199e95391122b1f9c901
Damian Molinski <tedeer@gmail.com>
2021-04-30 23:56:41 +0200

    General explanation of the way

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-let number = 0;
+/**
+ * Kilka przydatnych definicji (wskazówek) jak możemy rozumieć domnięcia (closures).
+ * * 1. Closure (domknięcie) to mechanizm, który pomaga Nam uzyskać dostęp do danej wartości poza funkcją i gdy ta funkcja nie jest już aktywna.
+ *
+ * * 2. Closure - zmienna istnieje w innej funkcji, mimo to, że funkcja, w której była stworzona ta zmienna nie jest już aktywna.
+ *
+ * * 3. W pamięci programu zmienna istnieje jeśli prowadzi do niej referencja z innej funckji. Możemu mieć do niej dost,ep, ale także je zmieniać.
+ */
 
-const add = () => {
-  number++;
-  document.body.textContent = `Aktualny stan licznika to: ${number}`;
+// example 1.
+function x() {
+  let number = 0;
+
+  function y() {
+    console.log(number);
+  }
+  return y
 }
 
-window.addEventListener('click', add);
+const example = x();
+
+example();
